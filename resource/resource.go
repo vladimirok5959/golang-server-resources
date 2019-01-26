b9037afe82dbe9e5d591e52e32075eb5308d0151
Vova Tkach <vladimirok5959@gmail.com>
2019-01-26 18:03:26 +0200

    Optimisation

--- resource/resource.go
+++ resource/resource.go
@@ -7,40 +7,60 @@ import (
 type Resource struct {
 	Path  string
 	Ctype string
-	bytes []byte
+	Bytes []byte
 }
 
 type resource struct {
-	list []Resource
+	maxurl int
+	list   map[string]Resource
 }
 
 func New() *resource {
-	r := resource{}
-	r.list = make([]Resource, 0)
+	r := resource{maxurl: 0}
+	r.list = map[string]Resource{}
 	return &r
 }
 
 func (this *resource) Add(path string, ctype string, bytes []byte) {
-	this.list = append(this.list, Resource{
+	// Do not add if already in resources list
+	if _, ok := this.list[path]; ok == true {
+		return
+	}
+
+	// Add to resources list
+	this.maxurl = len(path)
+	this.list[path] = Resource{
 		Path:  path,
 		Ctype: ctype,
-		bytes: bytes,
-	})
+		Bytes: bytes,
+	}
 }
 
 func (this *resource) Response(w http.ResponseWriter, r *http.Request, before func(w http.ResponseWriter, r *http.Request, i *Resource), after func(w http.ResponseWriter, r *http.Request, i *Resource)) bool {
-	for _, value := range this.list {
-		if r.URL.Path == "/"+value.Path {
-			if before != nil {
-				before(w, r, &value)
-			}
-			w.Header().Set("Content-Type", value.Ctype)
-			w.Write(value.bytes)
-			if after != nil {
-				after(w, r, &value)
-			}
-			return true
-		}
+	// Do not process if this is not necessary
+	if len(r.URL.Path) <= 1 || len(r.URL.Path)-1 > this.maxurl {
+		return false
 	}
-	return false
+
+	// Check for resource
+	res, ok := this.list[r.URL.Path[1:]]
+	if ok == false {
+		return false
+	}
+
+	// Call `before` callback
+	if before != nil {
+		before(w, r, &res)
+	}
+
+	// Send resource
+	w.Header().Set("Content-Type", res.Ctype)
+	w.Write(res.Bytes)
+
+	// Call `after` callback
+	if after != nil {
+		after(w, r, &res)
+	}
+
+	return true
 }
